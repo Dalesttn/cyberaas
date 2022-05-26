f012dd8496a00b35bb530d3ccebbc45826ae5bbe
Dale Sutton <dalesttn@gmail.com>
2022-05-26 14:27:33 +0800

    About us page

---
+++ content/about/_index.md
@@ -0,0 +1,5 @@
+---
+title: About Us
+subheading: "About us Overview"
+summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vestibulum est ut nulla pellentesque, a tempus lorem bibendum."
+---
--- themes/apsho/assets/js/script.js
+++ themes/apsho/assets/js/script.js
@@ -125,10 +125,10 @@ $(window).on("scroll", function () {
 });
 
 function initMap() {
-
-  var macc = {lat: 42.1382114, lng: -71.5212585};
-  var map = new google.maps.Map(
-  document.getElementById('googlemap'), {zoom: 15, center: macc});
-  var marker = new google.maps.Marker({position: macc, map: map});
-
+  var macc = { lat: 42.1382114, lng: -71.5212585 };
+  var map = new google.maps.Map(document.getElementById("googlemap"), {
+    zoom: 15,
+    center: macc,
+  });
+  var marker = new google.maps.Marker({ position: macc, map: map });
 }
